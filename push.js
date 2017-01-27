bbf0bba00603e673a9089ba7b88a4dcab2c9b51f
peteranny <peteranny2@gmail.com>
2017-01-27 11:34:37 +0800

    fix push

--- push.js
+++ push.js
@@ -1,53 +1,51 @@
 const https = require('https');
 const lib = require('./lib');
 
-module.exports = function sendPush(to, message, acc_tok){
-    return new Promise(function(resolve, reject){
-        const data = {
-            to: to,
-            messages: [{
-                type: 'text',
-                text: message,
-            }],
-        };
-        const body = JSON.stringify(data);
-        const options = {
-            host: 'api.line.me',
-            path: '/v2/bot/message/reply',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(body),
-                'Authorization': 'Bearer ' + acc_tok,
-            },
-        };
-        const req = https.request(options, function(res) {
-            const buf = [], n = 0;
-            res.on('data', function(chunk) {
-                buf.push(chunk);
-            });
-            res.on('end', function() {
-                try{
-                    const json = JSON.parse(Buffer.concat(buf).toString());
-                    if(lib.isEmptyObject(json)){
-                        resolve();
-                    }
-                    else{
-                        throw new Error(json.message);
-                    }
-                } catch(err) {
-                    reject(err);
-                }
-            })
-            res.on('error', function(err) {
-                reject(err);
-            });
+module.exports = function sendPush(to, message, acc_tok, next){
+    const data = {
+        to: to,
+        messages: [{
+            type: 'text',
+            text: message,
+        }],
+    };
+    const body = JSON.stringify(data);
+    const options = {
+        host: 'api.line.me',
+        path: '/v2/bot/message/reply',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(body),
+            'Authorization': 'Bearer ' + acc_tok,
+        },
+    };
+    const req = https.request(options, function(res) {
+        const buf = [], n = 0;
+        res.on('data', function(chunk) {
+            buf.push(chunk);
         });
-        req.write(body);
-        req.end();
-        req.on('error', function(e) {
-            reject(e);
+        res.on('end', function() {
+            try{
+                const json = JSON.parse(Buffer.concat(buf).toString());
+                if(lib.isEmptyObject(json)){
+                    next();
+                }
+                else{
+                    throw new Error(json.message);
+                }
+            } catch(err) {
+                next(err);
+            }
         })
+        res.on('error', function(err) {
+            next(err);
+        });
     });
+    req.write(body);
+    req.end();
+    req.on('error', function(e) {
+        next(e);
+    })
 }
 
