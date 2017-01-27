7a7294eb8fedb56309710ff904a54ab1bf5f35c5
peteranny <peteranny2@gmail.com>
2017-01-27 13:21:22 +0800

    sel-role

--- handle-message.js
+++ handle-message.js
@@ -1,6 +1,31 @@
+const user_stages = {};
+
+function Stage(userId, push){
+    this.stage = 'init';
+    this.next = function(input){
+        switch(this.stage){
+            case 'init':
+                this.stage = 'sel-role';
+                break;
+            default:
+                console.log('Unknown stage='+this.stage);
+        }
+    }
+    this.reply = function(){
+        switch(this.stage){
+            case 'sel-role':
+                return ['請問你要當誰?', '(1) 小風', '(2) 馬兒'].join('\n');
+            default:
+                return 'Unknown stage='+this.stage;
+        }
+    }
+}
+
 module.exports = function(userId, message, push){
-    setTimeout(function(){
-        push(message);
-    }, 1000);
-    return 'OK';
+    if(!user_stages[userId]){
+        user_stages[userId] = new Stage(userId, push);
+    }
+    const stage = user_stages[userId];
+    stage.next(message);
+    return stage.reply();
 }
