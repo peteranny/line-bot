1aa40384a1e7b6ed58b4c8992f28fd7ae808d6ba
peteranny <peteranny2@gmail.com>
2017-01-27 13:29:03 +0800

    confirm-start/exit

--- handle-message.js
+++ handle-message.js
@@ -5,7 +5,20 @@ function Stage(userId, push){
     this.next = function(input){
         switch(this.stage){
             case 'init':
-                this.stage = 'sel-role';
+                this.stage = 'confirm-start';
+                break;
+            case 'confirm-start':
+            case 'confirm-start-again':
+                if(input == '好'){
+                    this.stage = 'sel-role';
+                }
+                else if(input == '不好'){
+                    this.stage = 'exit';
+                }
+                else{
+                    this.stage = 'confirm-start-again';
+                }
+            case 'sel-role':
                 break;
             default:
                 console.log('Unknown stage='+this.stage);
@@ -13,8 +26,15 @@ function Stage(userId, push){
     }
     this.reply = function(){
         switch(this.stage){
+            case 'confirm-start':
+                return '和小風馬兒一起玩好不好?';
+            case 'confirm-start-again':
+                return '請說「好」或「不好」';
             case 'sel-role':
                 return ['請問你要當誰?', '(1) 小風', '(2) 馬兒'].join('\n');
+            case 'exit':
+                delete user_stages[userId];
+                return '小風小馬跟你說掰掰!';
             default:
                 return 'Unknown stage='+this.stage;
         }
